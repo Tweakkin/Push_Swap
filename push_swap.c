0eee2a8b5614e8f8d39d1701d2e39a105ac8888a
yd7w <yahyaboukhmira7@gmail.com>
2025-08-03 21:24:02 +0200

    still working on it

--- push_swap.c
+++ push_swap.c
@@ -21,13 +21,17 @@ int main(int argc, char **argv)
     int j = 0;
     data.a_stack = NULL;
     char **temp_arr;
+    if (argc < 2)
+        exit_error(); 
     while (i < argc)
     {
         temp_arr = ft_split(argv[i], ' ');
+        check_for_emptystring(argv[i]);
+        check_for_invalidchar(argv[i]);
         while (temp_arr[j] != NULL)
         {
-            check_for_duplicates(ft_atoi(temp_arr[j]), data.a_stack);
-            ft_lstadd_back(&data.a_stack, create_newnode(ft_atoi(temp_arr[j])));
+            check_for_duplicates(ft_atoi_safe(temp_arr[j]), data.a_stack);
+            ft_lstadd_back(&data.a_stack, create_newnode(ft_atoi_safe(temp_arr[j])));
             j++;
         }
         j = 0;
--- push_swap.h
+++ push_swap.h
@@ -1,5 +1,8 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
 
 typedef struct s_stack
@@ -13,7 +16,8 @@ typedef struct s_data
     t_stack *a_stack;
 }t_data;
 
-int	ft_atoi(const char *str);
+int	ft_atoi_safe(const char *str);
+void check_for_invalidchar(char *arg);
 void exit_error();
 t_stack    *ft_lstlast(t_stack *lst);
 t_stack *create_newnode(int value);
@@ -22,3 +26,7 @@ void	ft_lstadd_front(t_stack **lst, t_stack *new);
 void    ft_lstadd_back(t_stack **lst, t_stack *new);
 void    check_for_duplicates(int num, t_stack *stack);
 char	**ft_split(char const *s, char c);
+int	ft_strlen(const char *str);
+void check_for_emptystring(char *arg);
+
+#endif
