c056b73e809da0f0ace1f27b67625af224bb02c4
yd7w <yahyaboukhmira7@gmail.com>
2025-08-03 21:24:30 +0200

    added

--- validate.c
+++ validate.c
@@ -14,3 +14,42 @@ void    check_for_duplicates(int num, t_stack *stack)
         current_node = current_node->next;
     }
 }
+
+void check_for_emptystring(char *arg)
+{
+    int i;
+
+    i = 0;
+    if (ft_strlen(arg) == 0)
+        exit_error();
+    else if (ft_strlen(arg) > 0)
+    {
+        while (i < ft_strlen(arg))
+        {
+            if (arg[i] != ' ')
+                return;
+            i++;
+        }
+    }
+    exit_error();
+}
+
+void check_for_invalidchar(char *arg)
+{
+    int i;
+
+    i = 0;
+    while (arg[i] != '\0')
+    {
+        if ((arg[i] >= '0' && arg[i] <= '9') || arg[i] == ' ')
+            ;
+        else if (arg[i] == '+' || arg[i] == '-')
+        {
+            if (!(arg[i-1] == ' ' || i == 0) || !(arg[i+1] <= '9' && arg[i+1] >= '0'))
+                exit_error();
+        }
+        else
+            exit_error();
+        i++;
+    }
+}
