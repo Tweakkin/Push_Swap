945bc5887e792666e4005b07e0abe8a01242623b
yd7w <yahyaboukhmira7@gmail.com>
2025-08-05 21:05:04 +0200

    added

--- operations.c
+++ operations.c
@@ -2,8 +2,24 @@
 
 int ss(t_data *a_data, t_data *b_data)
 {
-    sa(b);
-    sb(b);
+    sa(a_data);
+    sb(b_data);
     write(1, "ss\n", 3);
+    return (1);
+}
+
+int rr(t_data *a_data, t_data *b_data)
+{
+    ra(a_data);
+    rb(b_data);
+    write(1, "rr\n", 3);
+    return(1);
+}
+
+int rrr(t_data *a_data, t_data *b_data)
+{
+    rra(a_data);
+    rrb(b_data);
+    write(1, "rrr\n", 4);
     return (1);
 }
