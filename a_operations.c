70dae8c3594697918fe6b5afee2aca9bcb2ea37f
yd7w <yahyaboukhmira7@gmail.com>
2025-08-05 21:04:25 +0200

    changes made

--- a_operations.c
+++ a_operations.c
@@ -12,3 +12,45 @@ int sa(t_data *a_data)
     write(1, "sa\n", 3);
     return(1);
 }
+
+int pa(t_data *a_data, t_data *b_data)
+{
+    t_stack *temp;
+
+    if (!b_data || !b_data->stack)
+        return (0);
+    temp = b_data->stack;
+    b_data->stack = b_data->stack->next;
+    ft_lstadd_front(&a_data->stack, temp);
+    write(1, "pa\n", 3);
+    return (1);
+}
+
+int ra(t_data *a_data)
+{
+    t_stack *temp;
+
+    if (!a_data || !a_data->stack || !a_data->stack->next)
+        return (0);
+    temp = a_data->stack;
+    a_data->stack = a_data->stack->next;
+    temp->next = NULL;
+    ft_lstadd_back(&a_data->stack, temp);
+    write(1, "ra\n", 3);
+    return (1);
+}
+
+int rra(t_data *a_data)
+{
+    t_stack *temp;
+    t_stack *last;
+
+    if (!a_data || !a_data->stack || !a_data->stack->next)
+        return (0);
+    last = ft_lstlast(a_data->stack);
+    temp = ft_secondlast(a_data->stack);
+    temp->next = NULL;
+    ft_lstadd_front(&a_data->stack, last);
+    write(1, "rra\n", 4);
+    return (1);
+}
--- b_operations.c
+++ b_operations.c
@@ -12,3 +12,45 @@ int sb(t_data *b_data)
     write(1, "sb\n", 3);
     return(1);
 }
+
+int pb(t_data *a_data, t_data *b_data)
+{
+    t_stack *temp;
+
+    if (!a_data || !a_data->stack)
+        return (0);
+    temp = a_data->stack;
+    a_data->stack = a_data->stack->next;
+    ft_lstadd_front(&b_data->stack, temp);
+    write(1, "pb\n", 3);
+    return (1);
+}
+
+int rb(t_data *b_data)
+{
+    t_stack *temp;
+
+    if (!b_data || !b_data->stack || !b_data->stack->next)
+        return (0);
+    temp = b_data->stack;
+    b_data->stack = b_data->stack->next;
+    temp->next = NULL;
+    ft_lstadd_back(&b_data->stack, temp);
+    write(1, "rb\n", 3);
+    return (1);
+}
+
+int rrb(t_data *b_data)
+{
+    t_stack *temp;
+    t_stack *last;
+
+    if (!b_data || !b_data->stack || !b_data->stack->next)
+        return (0);
+    last = ft_lstlast(b_data->stack);
+    temp = ft_secondlast(b_data->stack);
+    temp->next = NULL;
+    ft_lstadd_front(&b_data->stack, last);
+    write(1, "rrb\n", 4);
+    return (1);
+}
