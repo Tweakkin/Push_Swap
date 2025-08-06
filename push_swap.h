a54f461a6e1d06801c6fb216021d8ec3ccfdc013
yd7w <yahyaboukhmira7@gmail.com>
2025-08-06 22:01:14 +0200

    working on it

--- push_swap.h
+++ push_swap.h
@@ -15,11 +15,12 @@ typedef struct s_stack
 typedef struct s_data
 {
     t_stack *stack;
+	char	**array;
 }t_data;
 
-int	ft_atoi_safe(const char *str);
-void check_for_invalidchar(char *arg);
-void exit_error();
+int	ft_atoi_safe(const char *str, t_data *a, t_data *b, char **argv);
+void check_for_invalidchar(char *arg, t_data *a, t_data *b, char **argv);
+void    exit_free(t_data *a, t_data *b, char **args);
 t_stack    *ft_lstlast(t_stack *lst);
 t_stack *create_newnode(int value);
 void print_list(t_stack *head);
@@ -28,10 +29,10 @@ int sb(t_data *b_data);
 int ss(t_data *a_data, t_data *b_data);
 void	ft_lstadd_front(t_stack **lst, t_stack *new);
 void    ft_lstadd_back(t_stack **lst, t_stack *new);
-void    check_for_duplicates(int num, t_stack *stack);
+void    check_for_duplicates(int num, t_stack *stack, t_data *a, t_data *b, char **argv);
 char	**ft_split(char const *s, char c);
 int	ft_strlen(const char *str);
-void check_for_emptystring(char *arg);
+void check_for_emptystring(char *arg, t_data *a, t_data *b, char **argv);
 int pa(t_data *a_data, t_data *b_data);
 int pb(t_data *a_data, t_data *b_data);
 int ra(t_data *a_data);
@@ -41,5 +42,10 @@ t_stack *ft_secondlast(t_stack *lst);
 int rra(t_data *a_data);
 int rrb(t_data *b_data);
 int rrr(t_data *a_data, t_data *b_data);
+void    init_data(t_data **a, t_data **b, char **argv);
+void    args_to_arr(t_data *a,t_data *b, int argc, char **argv);
+int is_sorted(t_data *a);
+void    free_arr(char **arr);
+void    free_stack_data(t_data *data);
 
 #endif
