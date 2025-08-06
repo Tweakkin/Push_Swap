934e4c9205ccc674104d587f337e1d0bb8aab74a
yd7w <yahyaboukhmira7@gmail.com>
2025-08-06 22:00:59 +0200

    working on it

--- push_swap.c
+++ push_swap.c
@@ -9,36 +9,49 @@ void print_list(t_stack *head) {
     printf("NULL\n");
 }
 
-void exit_error()
+void    init_data(t_data **a, t_data **b, char **argv)
 {
-    exit(1);
+    *a = malloc(sizeof(t_data));
+    if (!*a)
+        exit_free(*a, *b, argv);
+    (*a)->stack = NULL;
+    *b = malloc(sizeof(t_data));
+    if (!*b)
+        exit_free(*a, *b, argv);
+    (*b)->stack = NULL; 
+}
+
+void    args_to_arr(t_data *a,t_data *b, int argc, char **argv)
+{
+    int i;
+    int j;
+
+    i = 1;
+    j = 0;
+    while (i < argc)
+    {
+        a->array = ft_split(argv[i], ' ');
+        check_for_emptystring(argv[i], a, b, argv);
+        check_for_invalidchar(argv[i], a, b, argv);
+        while (a->array[j] != NULL)
+        {
+            check_for_duplicates(ft_atoi_safe(a->array[j], a, b, argv), a->stack, a, b, argv);
+            ft_lstadd_back(&a->stack, create_newnode(ft_atoi_safe(a->array[j], a, b, argv)));
+            j++;
+        }
+        j = 0;
+        i++;   
+    }
 }
 
 int main(int argc, char **argv)
 {
-    t_data *a = malloc(sizeof(t_data));
-    t_data *b = malloc(sizeof(t_data));
-    int i = 1;
-    int j = 0;
-    a->stack = NULL;
-    b->stack = NULL;
-    char **temp_arr;
+    t_data *a;
+    t_data *b;
+
     if (argc < 2)
-        exit_error(); 
-    while (i < argc)
-    {
-        temp_arr = ft_split(argv[i], ' ');
-        check_for_emptystring(argv[i]);
-        check_for_invalidchar(argv[i]);
-        while (temp_arr[j] != NULL)
-        {
-            check_for_duplicates(ft_atoi_safe(temp_arr[j]), a->stack);
-            ft_lstadd_back(&a->stack, create_newnode(ft_atoi_safe(temp_arr[j])));
-            j++;
-        }
-        j = 0;
-        i++;
-    }
-    rrr(a, b);
-    print_list(a->stack);
+        exit(1); 
+    init_data(&a, &b, argv);
+    args_to_arr(a, b, argc, argv);
+    printf("%d", is_sorted(a));
 }
